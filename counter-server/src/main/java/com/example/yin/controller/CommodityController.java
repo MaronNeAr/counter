b7ec3a41547878484e99d5906f90e6e7367457c9
MaronNeAr <97968316+MaronNeAr@users.noreply.github.com>
2022-10-08 21:40:14 +0800

    Add files via upload

--- counter-server/src/main/java/com/example/yin/controller/CommodityController.java
+++ counter-server/src/main/java/com/example/yin/controller/CommodityController.java
@@ -1,12 +1,16 @@
 package com.example.yin.controller;
 
 import com.example.yin.config.common.SuccessMessage;
+import com.example.yin.constant.Constants;
 import com.example.yin.domain.Commodity;
 import com.example.yin.service.CommodityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -14,6 +18,15 @@ import java.util.List;
 public class CommodityController {
     @Autowired
     CommodityService commodityService;
+
+    @Configuration
+    public static class MyPicConfig implements WebMvcConfigurer {
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/img/**")
+                    .addResourceLocations(Constants.IMAGES_PATH);
+        }
+    }
 
     @GetMapping("/goods/{id}")
     public Object getGoods(@PathVariable("id") Integer id) {
